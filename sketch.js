ab1ee5397c4f9f646329e20fa9a24b0feeb4e2e4
Cathryn Ploehn <ploehn.cathryn@gmail.com>
2018-11-15 00:32:47 -0500

    update

--- sketch.js
+++ sketch.js
@@ -1,59 +1,68 @@
 
-
-var x = 400, y = 300;
-var w = 100, h = 100;
 var tip = [];
-var pitch = 0.55;
-
-var colors = chroma.scale(['yellow', 'red', 'purple']);
-var colorScale = d3.scaleLinear()
-		.domain([-1,1])
-		.range([0,1]);
-
 for(i = 0; i < 5; i++){
 	tip.push({x:0, y:0});
 }
 
+var pitch = 0.55;
+var bigness = 10;
+
+var colors = chroma.scale(['yellow', 'red', 'black'])
+    .mode('lch');
+var colorScale = d3.scaleLinear()
+		.domain([-1,1])
+		.range([0,1]);
+
+var sizeScale = d3.scaleLinear()
+		.domain([-500,500])
+		.range([100, 5]);
+
+
 function setup() {
 	createCanvas(displayWidth, displayHeight);
-	background(0, 0, 0);
+	$('canvas').attr('id', 'canvas');
+	background(10, 10, 10);
 	fill(0,0,255);	
 }
 
 function draw() {
-	var color = colors(colorScale(pitch)).alpha(0.5);
+	var color = colors(colorScale(pitch));
 	color = color.rgb();
-	background(0, 0, 0, 1);
-	// console.log(color)
-	// color = chroma.random().rgb();
-
-
+	color.push(10); //opacity
 	fill(color);
+	
 	stroke(0,0,0,0);
 
 	for(i = 0; i < tip.length; i++){
-		ellipse(tip[i].x, tip[i].y, 50, 50 );
+		ellipse(tip[i].x, tip[i].y, bigness, bigness );
 	}
-
-	// ellipse(x, y, w, h);
 
 }
 
 
 Leap.loop(function(frame) {
-
-  frame.hands.forEach(function(hand, index) {
+	if(frame.hands.length == 0){
+		x = 0;
+		y = 0;
+		bigness = 0;
+	}
+  	frame.hands.forEach(function(hand, index) {
 		var pos = hand.screenPosition();
 		pitch = hand.pitch();
-		// console.log(pitch);
+		bigness = sizeScale(pos[2]);
 		x = pos[0];
 		y = pos[1];
+
 		hand.fingers.forEach(function(finger, index){
 			var pos = finger.screenPosition();
-			// console.log(pos);
 			tip[index].x = pos[0];
 			tip[index].y = pos[1];
 		});
   });
 
 }).use('screenPosition', {scale: 0.25});
+
+$(document).click(function(){
+	var canvas = document.getElementById('canvas');
+	var dataURL = canvas.toDataURL();
+});
